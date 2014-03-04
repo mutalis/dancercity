1ed346ba85996667fa8b47314a06c6f895a625ea
Mutalis <rodolfo@mutalis.com>
2014-03-04 13:57:36 -0600

    Dancer City Web App.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dancercity::Application.config.secret_key_base = '8d078bc64debbf522b9609c8c777282f384f06ede253817db2dfd895bfe7e499180246fb9b03d6b223ab49d81104d4044ba552b68d1fa26838e3f6e067bfb94b'
