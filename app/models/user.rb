b5ae55bfbe6799c3aec18aa671169209e5914f54
Mutalis <rodolfo@mutalis.com>
2014-03-12 17:53:17 -0600

    Sessions controller added to handle the Facebook callback. from_omniauth method added to the User model in order to store the user info. (omniauth hash ) obtained from FB.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
 
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.image = auth.info.image
+      user.email = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.gender = auth.extra.raw_info.gender
+      user.save!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Dancercity::Application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
