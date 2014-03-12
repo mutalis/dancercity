65a58f4628c0122b364b507fda5d319c51ba650c
Mutalis <rodolfo@mutalis.com>
2014-03-12 17:58:04 -0600

    Only the basic info from FB will be required the first time the FB user visit DancerCiy.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV!['FACEBOOK_KEY'], ENV!['FACEBOOK_SECRET'], scope: 'email, publish_stream'
+  provider :facebook, ENV!['FACEBOOK_KEY'], ENV!['FACEBOOK_SECRET'],
+            scope: 'basic_info', :display => 'page'
 end
