b0e23a315bb9a64a048edc0251fcee7fdc0d30aa
Mutalis <rodolfo@mutalis.com>
2014-03-20 12:53:43 -0600

    Scope close_to added in order to find users within a given ratio of 2 Km taking a coord as a reference. Test included.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
 
+  scope :close_to, -> (latitude, longitude, distance_in_meters = 2000) {
+    where(%{
+      ST_DWithin(
+        ST_GeographyFromText(
+          'SRID=4326;POINT(' || users.longitude || ' ' || users.latitude || ')'
+        ),
+        ST_GeographyFromText('SRID=4326;POINT(%f %f)'),
+        %d
+      )
+    } % [longitude, latitude, distance_in_meters])
+  }
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
@@ -8,11 +20,12 @@ class User < ActiveRecord::Base
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.image = auth.info.image
-      user.email = auth.info.email
+      # user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.gender = auth.extra.raw_info.gender
       user.save!
     end
   end
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "close users" do
+    far_user = User.create!(
+      username:  "Far user",
+      latitude:   40.000000,
+      longitude: -77.000000
+    )
+
+    close_user = User.create!(
+      username:"Close user",
+      latitude:   39.010000,
+      longitude: -75.990000
+    )
+
+    close_users = User.close_to(39.000000, -76.000000).load
+
+    assert_equal 1,          close_users.size
+    assert_equal close_user, close_users.first
+    puts close_users.inspect
+  end
 end
