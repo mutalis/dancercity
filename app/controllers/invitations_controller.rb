c701b046bba2bf0df161d8be03a9e7b16f45bb5b
rasg <rodolfo@mutalis.com>
2014-06-02 20:59:48 -0500

    A message can be added when an invitation is created.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,7 @@
 class InvitationsController < ApplicationController
   before_action :set_invitation, only: [:show, :update]
 
-  respond_to :js
+  # respond_to :js
 
   def index
     if current_user && (current_user.slug == params[:user_id])
@@ -16,13 +16,13 @@ class InvitationsController < ApplicationController
   end
   
   def create
-    invited_user = User.find(params[:user_id])
+    invited_user = User.friendly.find(params[:user_id])
     
     if current_user && invited_user
-      @invitation = current_user.sent_invitations.create!(user_id: invited_user.id, date: Time.now)
+      @invitation = current_user.sent_invitations.create!(user_id: invited_user.id, date: Time.now, message: params[:invitation][:message])
       # @invitation = current_user.sent_invitations.create!(invitation_params)
       # redirect_to :back, notice: "The invitation has been sent."
-      send_facebook_message(invited_user, @invitation)
+      send_facebook_message(invited_user, new_inv_message(@invitation))
       flash.now[:notice] = "The invitation to #{invited_user.first_name} has been sent."
     else
       raise "Create invitation error."
@@ -33,7 +33,8 @@ class InvitationsController < ApplicationController
     if (current_user.slug == params[:user_id]) && (params[:ans] == 'accepted' || params[:ans] == 'rejected')
 
      if @invitation.update(status: params[:ans])
-       flash.now[:notice] = "You've #{params[:ans]} the invitation from #{@invitation.partner.first_name} ."
+       send_facebook_message(@invitation.partner, response_inv_message(params[:ans]))
+       flash.now[:notice] = "You've #{params[:ans]} the invitation from #{@invitation.partner.first_name} #{@invitation.partner.last_name} ."
      end
     end
   end
@@ -44,12 +45,12 @@ class InvitationsController < ApplicationController
     @invitation = current_user.invitations.find(params[:id]) if current_user
   end
 
-  def send_facebook_message(receiver, invitation)
+  def send_facebook_message(receiver, message)
     
     receiver_chat_id   = "-#{receiver.uid}@chat.facebook.com"
     sender_chat_id   = "-#{receiver.uid}@chat.facebook.com"
 
-    jabber_message   = Jabber::Message.new(receiver_chat_id, facebook_message(invitation))
+    jabber_message   = Jabber::Message.new(receiver_chat_id, message)
     jabber_message.subject = "Dancer City"
   
     client = Jabber::Client.new(Jabber::JID.new(sender_chat_id))
@@ -62,10 +63,19 @@ class InvitationsController < ApplicationController
     raise FacebookChatAccessDenied, "No access to Facebook Chat"
   end
 
-  def facebook_message(invitation)
-    "Dancer City invitation:\n
-    You got an invitation from #{current_user.first_name} #{current_user.last_name} to dance.\n
+  def new_inv_message(invitation)
+    "Dancer City notification:\n\n
+    You've got an invitation to dance from #{current_user.first_name} #{current_user.last_name}.\n\n
     Please see the details at: #{user_invitation_url(invitation.user, invitation)}"
+  end
+  
+  def response_inv_message(answer)
+    message = "Dancer City notification:\n\n #{current_user.first_name} #{current_user.last_name}"
+    if answer == 'rejected'
+      message += " has rejected your invitation to dance. You can try again in another time.\n"
+    else
+      message += " has accepted your invitation to dance.\n"
+    end
   end
   
 end
