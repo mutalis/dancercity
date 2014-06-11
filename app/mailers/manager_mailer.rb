2a31aaed6ebea522aa415062a74ba6b5e638043e
rasg <rodolfo@mutalis.com>
2014-06-11 02:57:54 -0500

    Don't set the reply_to in the emails because it's a no reply address.

--- app/mailers/manager_mailer.rb
+++ app/mailers/manager_mailer.rb
@@ -1,6 +1,5 @@
 class ManagerMailer < ActionMailer::Base
-  default from: ENV!['SMTP_USER'],
-          reply_to: ENV!['SMTP_USER']
+  default from: ENV!['SMTP_USER']
 
   def new_invitation(email, message)
     mail(to: email, subject: "You've got a new invitation in Dancer City",
