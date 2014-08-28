0e9b5750ceb21605c19a514aa83dc00f3df04466
rasg <rodolfo@mutalis.com>
2014-08-27 23:10:29 -0500

    Tangomexico page added in order to display the Tango Hoy page feed using the FB Graph API

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Dancercity::Application.routes.draw do
 
   get 'likes/:id', to: 'likes#create', as: 'likes'
 
+  get 'tangomexico' => 'pages#tangomexico'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
