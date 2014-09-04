1a0e146165bf4eca635799c57bc36acddb4c3573
rasg <rodolfo@mutalis.com>
2014-09-03 23:23:23 -0500

    BugFix: facebook omniauth session error when access the auth hash. The error was: ActiveModel::ForbiddenAttributesError. http://expertsoverflow.com/questions/25399414/rails-4-1-5-omniauth-strong-parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -92,7 +92,7 @@ class User < ActiveRecord::Base
   }
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       # if the FB username is not defined use the Uid as the username
