cfa6ba6e7fb9ed2ab1ecad02002fa48e7cb812e6
rasg <rodolfo@mutalis.com>
2014-09-25 14:00:52 -0500

    The Post entries are feed from the FB API instead of the RSS feed.

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,17 +19,35 @@ class Post < ActiveRecord::Base
   end
 
   def define_slug
-    if self.respond_to? :summary
-      summary[0..64].strip
+    if link_name.present?
+      link_name[0..64].strip
+    elsif message.present?
+      message[0..64].strip
     else
       SecureRandom.uuid
     end
   end
 
-  def self.add_from_feed(feed_url)
-    feed = Feedjira::Feed.fetch_and_parse(feed_url)
-    add_entries(feed.entries)
-    feed
+  def self.add_from_feed()
+    admin_user = User.find_by uid: '100005971752949'
+
+    if admin_user
+      # fb_page_id = 535306009870436
+      fb_page_id = 111998825520170
+
+      # permissions = admin_user.facebook.get_connections('me','permissions')
+      # @has_wallpost_permission = permissions[0]['publish_stream'].to_i == 1 ? true : false
+      # @has_manage_pages_permission = permissions[0]['manage_pages'].to_i == 1 ? true : false
+      # 
+      # if @has_wallpost_permission && @has_manage_pages_permission
+      #   page_token = admin_user.facebook.get_page_access_token(fb_page_id)
+      #   page_graph = Koala::Facebook::API.new(page_token)
+
+        # page_feed = page_graph.get_connection('me', 'feed')
+        page_feed = admin_user.facebook.get_connections(fb_page_id,'feed')
+        add_entries(page_feed)
+      # end
+    end
   end
 
   def self.add_from_feed_daemon(feed_url, delay_interval = 30.minutes)
@@ -115,8 +133,12 @@ class Post < ActiveRecord::Base
 
         # link_text = " Puedes ver esta publicación en: #{post_url(self)}"
         # message = self.convert_to_text + link_text
-
-        page_graph.put_connections(fb_page_id,'feed', link: post_url_value)
+        if self.description.present?
+          desc_text = self.description
+        else
+          desc_text = self.message
+        end
+        page_graph.put_connections(fb_page_id,'feed', link: post_url_value, picture: self.picture_url, description: desc_text)
       end
     end
   end
@@ -124,18 +146,45 @@ class Post < ActiveRecord::Base
   private
   def self.add_entries(entries)
     entries.each do |entry|
-      unless exists? entry_id: entry.entry_id
-        post = new(
-          entry_id: entry.entry_id,
-          title: entry.title,
-          url: entry.url,
-          summary: entry.summary,
-          published_at: entry.published,
-          user: User.first
-        )
+      unless exists? entry_id: entry['id']
 
-        MetaTag.create!(name: 'title', content: entry.title[0..69].strip, post: post)
-        MetaTag.create!(name: 'description', content: entry.summary[0..159].strip, post: post)
+        if entry.has_key? 'message'
+          text_message = entry['message']
+        else
+          text_message = ''
+        end
+
+        if (entry.has_key? 'picture') && (entry['picture'].present?)
+          picture_url = entry['picture']
+        else
+          picture_url = 'https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png'
+        end
+
+        post = new.tap do |new_post|
+          new_post.entry_id = entry['id']
+          new_post.caption = entry['caption']
+          new_post.published_at = entry['created_time'].to_time
+          new_post.description = entry['description']
+          new_post.link = entry['link']
+          new_post.message = text_message
+          new_post.link_name = entry['name']
+          new_post.picture_url = picture_url
+          new_post.video_url = entry['source']
+          new_post.status_type_desc = entry['status_type']
+          new_post.status_type = entry['type']
+
+          new_post.fb_permalink = "https://www.facebook.com/#{entry['id'].split('_')[0]}/posts/#{entry['id'].split('_')[1]}"
+          new_post.user = User.first
+        end
+
+        if post.description.present?
+          meta_text = post.description
+        else
+          meta_text = post.message
+        end
+
+        MetaTag.create!(name: 'title', content: meta_text[0..69].strip, post: post)
+        MetaTag.create!(name: 'description', content: meta_text[0..159].strip, post: post)
         MetaTag.create!(name: 'keywords', content: 'tango, mexico, tango mexico, clases tango, clases de tango, milonga, milongas, musica de tango, musica tango, bailar tango', post: post)
 
       end
