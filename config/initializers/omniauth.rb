93085982ca17582179d72c8e20ae247e5a12b19d
rasg <rodolfo@mutalis.com>
2014-12-24 00:16:24 -0600

    BigFix: Faraday::SSLError: SSL_connect returned=1 errno=0 state=SSLv3 read server certificate B: certificate verify failed.  Faraday (which Omniauth/Oauth use for their HTTP calls) is not setting the ca_path variable for OpenSSL. Since Faraday isn't setting this variable, OpenSSL isn't finding the CA certificate. It's required for the omniauth-facebook gem.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,14 @@
 OmniAuth.config.logger = Rails.logger
 
+cert_path = `openssl version -a`.match(%r~.*OPENSSLDIR: ("[a-zA-Z\/]+")~)[1]
+cert_path.gsub! /"/,''
+
+ca_file = cert_path + '/cert.pem'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV!['FACEBOOK_KEY'], ENV!['FACEBOOK_SECRET'],
-            scope: 'basic_info, email, user_location', :display => 'page'
+            scope: 'basic_info, email, user_location', :display => 'page',
+            :client_options => {
+              :ssl => {:ca_file => ca_file}
+             }
 end
